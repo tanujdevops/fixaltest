0ef8bf7501379bae000e079c679fee05d5b907f6
Tanuj <tanujsde@gmail.com>
2023-08-09 17:35:44 +0530

    Updated AboutUs svg

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-[#163452]`}>
+      <img src={icon} alt="star" className="object-contain" />
     </div>
     <div className="flex-1 flex flex-col ml-3">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
