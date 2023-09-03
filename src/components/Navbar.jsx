8f36653626b4cedbf4c89380dd701798d8c6efb0
Tanuj <tanujsde@gmail.com>
2023-09-03 23:14:27 +0530

    Logo Changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[180px] h-[80px]" />
+      <img src={logo} alt="hoobank" className="w-[160px] h-[70px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
